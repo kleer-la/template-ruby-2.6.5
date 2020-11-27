d01cbdf889c804715260c657dfa05455737009cc
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-11-27 09:20:06 -0600

    adivina la palabra

---
+++ adivina-la-palabra.rb
@@ -0,0 +1,22 @@
+class AdivinaLaPalabra
+        @palabraAAdivinar = ""
+    def getPalabraAAdivinar
+        return @palabraAAdivinar
+    end    
+    def ocultar_palabra(palabra)
+        #Iterador
+        i=0 
+        #Palabra en Asteriscos
+        palabraAsterisco = ""
+        @palabraAAdivinar = palabra
+            while i < palabra.size  do 
+                palabraAsterisco += "*"
+                i +=1
+            end
+        return palabraAsterisco
+    end 
+end
+
+#adivinaLaPalabra = AdivinaLaPalabra.new
+#adivinaLaPalabra.ocultar_palabra "Inglaterra"
+#puts adivinaLaPalabra.getPalabraAAdivinar
