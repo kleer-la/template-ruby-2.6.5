838d0b262b123d96aebe3ef17613073f4cc621e3
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-11-27 10:02:51 -0600

    refactor de clase

--- lib/adivina-la-palabra.rb
+++ lib/adivina-la-palabra.rb
@@ -1,26 +1,11 @@
 class AdivinaLaPalabra
-    @palabraAAdivinar = ""
-    def getPalabraAAdivinar
-        return @palabraAAdivinar
-    end    
+  
     def ocultar_palabra(palabra)
-        #Iterador
-        i=0 
-        #Palabra en Asteriscos
-        palabraAsterisco = ""
         @palabraAAdivinar = palabra
-            while i < palabra.size  do 
-                palabraAsterisco += "*"
-                i +=1
-            end
-        return palabraAsterisco
+        return "*" *palabra.size
     end 
 
     def esta_la_letra? (letra)
-        return true
+        return @palabraAAdivinar.include? letra
     end
 end
-
-#adivinaLaPalabra = AdivinaLaPalabra.new
-#adivinaLaPalabra.ocultar_palabra "Inglaterra"
-#puts adivinaLaPalabra.getPalabraAAdivinar
--- spec/adivina_la_palabra_spec.rb
+++ spec/adivina_la_palabra_spec.rb
@@ -8,12 +8,20 @@ describe AdivinaLaPalabra do
     end
     it "Convertir la palabra en asteriscos cantidad incorrecta" do
         adivinaLaPalabra = AdivinaLaPalabra.new
-        asteriscos = adivinaLaPalabra.ocultar_palabra "Inglaterra"
+        asteriscos = adivinaLaPalabra.ocultar_palabra "inglaterra"
         expect(asteriscos).not_to eq "*****"
     end
     it "la letra está en la palabra" do
         adivinaLaPalabra =  AdivinaLaPalabra.new
+        asteriscos = adivinaLaPalabra.ocultar_palabra "inglaterra"
         estaLaPalabra =  adivinaLaPalabra.esta_la_letra? "a"
-        expect(estaLaPalabra).to eq true
+        expect(estaLaPalabra).to eq 'inglaterra'
+    end
+
+    it "la letra no está en la palabra" do
+        adivinaLaPalabra =  AdivinaLaPalabra.new
+        asteriscos = adivinaLaPalabra.ocultar_palabra "inglaterra"
+        estaLaPalabra =  adivinaLaPalabra.esta_la_letra? "j"
+        expect(estaLaPalabra).not_to eq true
     end
 end
