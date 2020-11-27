78976a6c70bde276eb42092c6022c2ea2339df61
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-11-27 09:49:50 -0600

    cambio de archivo lib

--- spec/adivina_la_palabra_spec.rb
+++ spec/adivina_la_palabra_spec.rb
@@ -1,4 +1,4 @@
-require "./adivina-la-palabra.rb"
+require "./lib/adivina-la-palabra.rb"
 describe AdivinaLaPalabra do
     # Palabra secreta en astiriscos
     it "Convertir la palabra en asteriscos" do
@@ -10,5 +10,10 @@ describe AdivinaLaPalabra do
         adivinaLaPalabra = AdivinaLaPalabra.new
         asteriscos = adivinaLaPalabra.ocultar_palabra "Inglaterra"
         expect(asteriscos).not_to eq "*****"
+    end
+    it "la letra está en la palabra" do
+        adivinaLaPalabra =  AdivinaLaPalabra.new
+        estaLaPalabra =  adivinaLaPalabra.esta_la_letra? "a"
+        expect(estaLaPalabra).to eq true
     end
 end
